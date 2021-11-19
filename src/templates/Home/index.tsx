514641e69d11157a0370d24f59ba59c3c1f212f6
Saull Brandão <saull@outlook.com>
2021-11-19 11:11:38 -0300

    feat: create home page template

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,3 +1,52 @@
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
 export const HomeTemplate = (): JSX.Element => {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.content}>
+          <h2>Como utilizar hooks</h2>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <FiCalendar size={20} />
+            <time>15 mar 2021</time>
+
+            <FiUser size={20} />
+            <span>Joseph Oliveira</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <h2>Como utilizar hooks</h2>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <FiCalendar size={20} />
+            <time>15 mar 2021</time>
+
+            <FiUser size={20} />
+            <span>Joseph Oliveira</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <h2>Como utilizar hooks</h2>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <FiCalendar size={20} />
+            <time>15 mar 2021</time>
+
+            <FiUser size={20} />
+            <span>Joseph Oliveira</span>
+          </div>
+        </div>
+
+        <button onClick={() => null} type="button" className={styles.loadPosts}>
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
 };
